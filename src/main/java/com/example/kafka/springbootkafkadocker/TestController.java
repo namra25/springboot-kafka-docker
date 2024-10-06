491558fdbf63844fda2afb9f85f46ac240991ab0
Namratha A <namra@cisco.com>
2024-10-06 12:31:04 +0530

    single topic

--- src/main/java/com/example/kafka/springbootkafkadocker/TestController.java
+++ src/main/java/com/example/kafka/springbootkafkadocker/TestController.java
@@ -1,6 +1,7 @@
 package com.example.kafka.springbootkafkadocker;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,14 +9,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
-    private final com.example.kafka.springbootkafka.Producer producer;
+    private final com.example.kafka.springbootkafkadocker.Producer producer;
 
     @Autowired
-    public TestController(com.example.kafka.springbootkafka.Producer producer) {
+    public TestController(com.example.kafka.springbootkafkadocker.Producer producer) {
         this.producer = producer;
     }
-    @PostMapping("/publish")
-    public void messageToTopic(@RequestParam("message") String message){
+    @PostMapping("/publish/{message}")
+    public void messageToTopic(@PathVariable String message){
 
         this.producer.sendMessage(message);
 
